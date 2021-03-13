8267f61069e9c85e7db12dd1fa737682d5385106
kitt3911 <ivanignatenko28@gmail.com>
2021-03-13 13:55:14 +0600

    auth service and user repository added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { AuthServiceInterface } from 'src/interfaces/auth.service.interface';
+import { CreateUserDto } from 'src/user/dto/create-user-dto';
 import { UserService } from 'src/user/user.service';
+import { User } from 'src/user/user/user.entity';
 
 @Injectable()
-export class AuthService {
+export class AuthService implements AuthServiceInterface {
+    constructor(
+        private usersService: UserService,
+        private jwtService: JwtService
+    ) { }
+
+    async login(createUserDto: CreateUserDto): Promise<any> {
+        const payload = { username: createUserDto.email, sub: createUserDto.password }
+        return {
+            access_token: this.jwtService.sign(payload)
+        }
+    }
+    async validateUser(createUserDto: CreateUserDto): Promise<any> {
+        const user = await this.usersService.findOneByEmail(createUserDto.email)
+        if (user && user.password === createUserDto.password) {
+            const { password, ...result } = user;
+            return result;
+        }
+        return null;
+    }
 }
--- src/interfaces/auth.service.interface.ts
+++ src/interfaces/auth.service.interface.ts
@@ -2,8 +2,8 @@ import { CreateUserDto } from "src/user/dto/create-user-dto";
 import { User } from "src/user/user/user.entity";
 
 export interface AuthServiceInterface {
-     register(createUserDto: CreateUserDto): Promise<User>;
+    // register(createUserDto: CreateUserDto): Promise<User>;
      login(createUserDto: CreateUserDto): Promise<any>;
-     validateUser(createUserDto: CreateUserDto): void;
+     validateUser(createUserDto: CreateUserDto): Promise<any>;
 }
 
